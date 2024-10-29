a41897f4c722e36cae5ca6161ae2cc3c0c03b757
vikaskushwaha <vikas.vikas.kushwaha@gmail.com>
2024-10-30 01:46:59 +0530

    pedmas logic complete and ui fix

--- src/Components/BasicCalulator/calulator.jsx
+++ src/Components/BasicCalulator/calulator.jsx
@@ -10,74 +10,57 @@ function MainCalDesign() {
     function clearScreen() {
         console.log(number);
         SetNumber('');
+        SetResult(0)
     }
     function equalTo(num) {
-        SetNumber(number + num);
-        // let allNo = [];
-        // let str = "";
-        // for (let i = 0; i < number.length; i++) {
-        //     console.log(number[i]);
-        //     if (number[i] === '+' || number[i] === '-' || number[i] === '*' || number[i] === '%' || number[i] === '/' || number === '=') {
-        //         allNo.push(parseInt(str));
-        //         allNo.push(number[i]);
-        //         str = '';
-        //     }
-        //     else {
-        //         str += number[i];
-        //     }
-        // }
-        // allNo.push(parseInt(str));
-        // console.log(allNo);
-        Pedmas();
-
-
-
-
+        let str = number;
+        // console.log(Pedmas(str));
+        SetResult(Pedmas(str));
     }
 
     return (
         <div className="flex items-center flex-col gap-y-6 align-middle  justify-center content-center">
-            <h1>Calulator</h1>
+            <h1 className="text-lg text-sky-400">Calulator</h1>
             <div className="flex flex-col items-center">
                 <div className="flex flex-col  border p-2">
-                    <div className="flex flex-row-reverse w-52 content-center ">
+                    <div className="flex flex-row-reverse w-80 h-10 content-center ">
                         {number}
                     </div>
-                    <div className="flex flex-row-reverse">{result}</div>
+                    <div className="flex flex-row-reverse font-bold w-80">{result}</div>
                 </div>
 
 
-                <table className="border-spacing-4 border-separate" >
-                    <tbody className="gap-y-2">
-                        <tr >
-                            <td className="rounded-md bg-slate-200 w-10" onClick={() => clearScreen()}><p className="text-green-300 font-medium m-2">AC</p></td>
-                            <td className="rounded-md bg-slate-200 w-10"><p className="text-green-300 font-medium m-2">+/-</p></td>
-                            <td className="rounded-md bg-slate-200 w-10"><p className="text-green-300 font-medium m-2">%</p></td>
-                            <td className="rounded-md bg-slate-200 w-10"><p className="text-green-300 font-medium m-2">\</p></td>
+                <table className=" table-auto border-spacing-4 border-separate" >
+                    <tbody className="flex flex-col gap-y-4">
+                        <tr className="flex flex-row gap-x-2">
+                            <td className="rounded-md bg-slate-200 w-20 flex justify-center items-center h-10 " onClick={() => clearScreen()}><p className="text-green-300 font-bold">AC</p></td>
+                            <td className="rounded-md bg-slate-200 w-20 flex justify-center items-center h-10  "><p className="text-green-300 font-medium">+/-</p></td>
+                            <td className="rounded-md bg-slate-200 w-20 flex justify-center items-center  h-10 " onClick={() => handleClick('%')}><p className="text-green-300 font-bold ">%</p></td>
+                            <td className="rounded-md bg-slate-200 w-20 flex justify-center items-center h-10 " onClick={() => handleClick('/')}><p className="text-green-300 font-bold">\</p></td>
                         </tr>
-                        <tr>
-                            <td className="rounded-md   bg-slate-200 w-10" onClick={() => handleClick(9)} ><p className="font-normal m-2">9</p></td>
-                            <td className="rounded-md   bg-slate-200 w-10" onClick={() => handleClick(8)}><p className="font-normal m-2">8</p></td>
-                            <td className="rounded-md   bg-slate-200 w-10" onClick={() => handleClick(7)}><p className="font-normal m-2">7</p></td>
-                            <td className="rounded-md   bg-slate-200 w-10" onClick={() => handleClick('+')}><p className="font-normal m-2">+</p></td>
+                        <tr className="flex flex-row gap-x-2">
+                            <td className="rounded-md   bg-slate-200 w-20 flex justify-center items-center h-10" onClick={() => handleClick(9)} ><p className="font-normal ">9</p></td>
+                            <td className="rounded-md   bg-slate-200 w-20 flex justify-center items-center h-10" onClick={() => handleClick(8)}><p className="font-normal ">8</p></td>
+                            <td className="rounded-md   bg-slate-200 w-20 flex justify-center items-center h-10" onClick={() => handleClick(7)}><p className="font-normal ">7</p></td>
+                            <td className="rounded-md   bg-slate-200 w-20 flex justify-center items-center h-10" onClick={() => handleClick('+')}><p className="font-normal">+</p></td>
                         </tr>
-                        <tr>
-                            <td className="rounded-md  bg-slate-200 w-10" onClick={() => handleClick(6)}><p className="font-normal m-2">6</p></td>
-                            <td className="rounded-md  bg-slate-200 w-10" onClick={() => handleClick(5)}><p className="font-normal m-2">5</p></td>
-                            <td className="rounded-md  bg-slate-200 w-10" onClick={() => handleClick(4)}><p className="font-normal m-2">4</p></td>
-                            <td className="rounded-md  bg-slate-200 w-10" onClick={() => handleClick('-')}><p className="font-normal m-2">-</p></td>
+                        <tr className="flex flex-row gap-x-2">
+                            <td className="rounded-md  bg-slate-200 w-20 flex justify-center items-center h-10" onClick={() => handleClick(6)}><p className="font-normal ">6</p></td>
+                            <td className="rounded-md  bg-slate-200 w-20 flex justify-center items-center h-10" onClick={() => handleClick(5)}><p className="font-normal ">5</p></td>
+                            <td className="rounded-md  bg-slate-200 w-20 flex justify-center items-center h-10" onClick={() => handleClick(4)}><p className="font-normal ">4</p></td>
+                            <td className="rounded-md  bg-slate-200 w-20 flex justify-center items-center h-10" onClick={() => handleClick('-')}><p className="font-normal">-</p></td>
                         </tr>
-                        <tr>
-                            <td className="rounded-md  bg-slate-200 w-10"><p className="font-normal m-2" onClick={() => handleClick(3)}>3</p></td>
-                            <td className="rounded-md  bg-slate-200 w-10"><p className="font-normal m-2" onClick={() => handleClick(2)}>2</p></td>
-                            <td className="rounded-md  bg-slate-200 w-10"><p className="font-normal m-2" onClick={() => handleClick(1)}>1</p></td>
-                            <td className="rounded-md  bg-slate-200 w-10"><p className="font-normal m-2" onClick={() => handleClick('*')}>*</p></td>
+                        <tr className="flex flex-row gap-x-2">
+                            <td className="rounded-md  bg-slate-200 w-20 flex justify-center items-center h-10"><p className="font-normal " onClick={() => handleClick(3)}>3</p></td>
+                            <td className="rounded-md  bg-slate-200 w-20 flex justify-center items-center h-10"><p className="font-normal " onClick={() => handleClick(2)}>2</p></td>
+                            <td className="rounded-md  bg-slate-200 w-20 flex justify-center items-center h-10"><p className="font-normal " onClick={() => handleClick(1)}>1</p></td>
+                            <td className="rounded-md  bg-slate-200 w-20 flex justify-center items-center h-10"><p className="font-normal " onClick={() => handleClick('*')}>*</p></td>
                         </tr>
-                        <tr>
-                            <td className="rounded-md  bg-slate-200 w-10"><p className="font-normal m-2" onClick={() => handleClick()}>Prev</p></td>
-                            <td className="rounded-md  bg-slate-200 w-10"><p className="font-normal m-2" onClick={() => handleClick(0)}>0</p></td>
-                            <td className="rounded-md  bg-slate-200 w-10"><p className="font-normal m-2" onClick={() => handleClick('.')}>.</p></td>
-                            <td className="rounded-md  bg-slate-200 w-10"><p className="font-normal m-2" onClick={() => equalTo('=')}>=</p></td>
+                        <tr className="flex flex-row gap-x-2">
+                            <td className="rounded-md  bg-slate-200 w-20 flex justify-center items-center h-10"><p className="font-normal " onClick={() => handleClick()}>Prev</p></td>
+                            <td className="rounded-md  bg-slate-200 w-20 flex justify-center items-center h-10"><p className="font-normal " onClick={() => handleClick(0)}>0</p></td>
+                            <td className="rounded-md  bg-slate-200 w-20 flex justify-center items-center h-10"><p className="font-normal " onClick={() => handleClick('.')}>.</p></td>
+                            <td className="rounded-md  bg-slate-200 w-20 flex justify-center items-center h-10"><p className="font-normal " onClick={() => equalTo('=')}>=</p></td>
                         </tr>
                     </tbody>
                 </table>
--- src/Components/logic/pedmas.js
+++ src/Components/logic/pedmas.js
@@ -17,15 +17,50 @@ class Stack {
     isEmpty() {
         return this.items.length === 0;
     }
+    printStk() {
+        return this.items
+    }
 }
 
-
-
-function Pedmas() {
+function isDigit(idx, str) {
+    const char = str.charAt(idx);
+    return char >= '0' && char <= '9';
+}
+function Pedmas(str) {
     let stack = new Stack();
-    console.log(stack.isEmpty());
-    console.log(stack.pop())
+    let ans = 0;
+    console.log(str.length);
 
+    if (str.length === 0) {
+        return ans;
+    }
+    let currentNumber = 0;
+    let operation = '+';
+    for (let i = 0; i < str.length; i++) {
+        let currentChar = str[i];
+        if (isDigit(i, str)) {
+            currentNumber = (currentNumber * 10) + (currentChar - '0');
+        }
+        if (!isDigit(i, str) || (i === (str.length - 1))) {
+            if (operation === '-') stack.push(-currentNumber);
+            else if (operation === '+') stack.push(currentNumber);
+            else if (operation === '*') {
+                stack.push(stack.pop() * currentNumber)
+            }
+            else if (operation === '/') {
+                stack.push(stack.pop() / currentNumber)
+            }
+            operation = currentChar;
+            currentNumber = 0;
+        }
+    }
+    console.log(stack.printStk());
+    while (!stack.isEmpty()) {
+        ans += stack.pop();
+    }
+    console.log(ans);
+    return ans;
 }
-
 export default Pedmas;
+
+
